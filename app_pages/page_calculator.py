288dbb0894545aea1318c129987a3442f09d44f4
Marcus F <marcusf.dev@gmail.com>
2024-11-03 15:34:11 +0000

    Add: Calculator Page to Streamlit app.

--- app_pages/page_calculator.py
+++ app_pages/page_calculator.py
@@ -1,2 +1,34 @@
+import streamlit as st
 
-    
+
+def calculator_body():
+    st.write("---")
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        num1 = st.number_input(label='Enter the first integer', step=1)
+    with col2:
+        num2 = st.number_input(label='Enter the second integer', step=2)
+    with col3:
+        operator = st.selectbox(
+            label='Select an operator',
+            options=['Add', 'Subtract', 'Multiply', 'Divide'])
+    if st.button('Click here to calculate!'):
+        if num2 == 0 and operator == 'Divide':
+            st.error('Division by zero error. Enter a non-zero interger.')
+        else:
+            calculator_function(num1, num2, operator)
+
+
+# In a real production enviroment this functionality should be placed in
+# seperate python script for concern seperation.
+def calculator_function(num1, num2, operator):
+    if operator == 'Add':
+        result = num1 + num2
+    elif operator == 'Subtract':
+        result = num1 - num2
+    elif operator == 'Multiply':
+        result = num1 * num2
+    elif operator == 'Divide':
+        result = num1 / num2
+
+    st.success(f'The Result is: **{result}**')
